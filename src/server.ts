1cef17a851c14740cfb0df2d4d62ef4043ecac27
GuilhermeLou22 <guilhermesilva@acad.ifma.edu.br>
2023-06-18 18:06:13 -0300

    register memories routes and cors

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,17 @@
-import fastify from 'fastify'
+import cors from '@fastify/cors';
+import fastify from 'fastify';
 
-const app = fastify()
+import { memoriesRoutes } from './routes/memories';
 
+const app = fastify();
+
+app.register(memoriesRoutes);
+
+app.register(cors);
 app
   .listen({
     port: 3333,
   })
   .then(() => {
-    console.log('🚀 HTTP server running on http://localhost:3333')
-  })
+    console.log('🚀 HTTP server running on http://localhost:3333');
+  });
